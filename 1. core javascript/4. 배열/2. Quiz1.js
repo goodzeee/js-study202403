811d67ac606167556c4d03aad2dd64bcbd4d7708
jihye <qkdtmdqo14@naver.com>
2024-03-25 12:47:44 +0900

    # 배열 기초 & 퀴즈 학습 완료

---
+++ 1. core javascript/4. 배열/2. Quiz1.js
@@ -0,0 +1,8 @@
+// 총점 : 000, 평균 : 000 나오게 하기
+// var scores = [83, 99, 100, 57];
+// var total = 0;
+// for (var total = 0; total < scores.length; total++) {
+//   total += scores[i];  // 총점 누적시키기 +=
+// }
+// var ave = total / scores.length;
+  
