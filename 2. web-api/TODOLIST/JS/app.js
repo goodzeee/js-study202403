c464ce51ed8d0c9e4733625cf3780d11a80c1d4e
jihye <qkdtmdqo14@naver.com>
2024-04-03 18:18:03 +0900

    # todolist javascript 완성

--- 2. web-api/TODOLIST/JS/app.js
+++ 2. web-api/TODOLIST/JS/app.js
@@ -62,12 +62,12 @@ function isValidate ($textInput) {
   const text = $textInput.value; // 태그 가져와서 유효하지 않으면 나올 css 만들기
   // 입력값이 비었거나 10글자 이상이면 유효하지 않음.
   if (text.trim() === '' || text.length > 10) {
-    $textInput.style.backgroud = 'orangered';
+    $textInput.style.backgroundColor = 'orangered';
     $textInput.setAttribute('placeholder', '필수 입력사항입니다. (10자 이내)');
     $textInput.value = '';
     return false;  // 입력 안되게 !
   }
-    $textInput.style.backgroud = '';
+    $textInput.style.backgroundColor = '';
     $textInput.setAttribute('placeholder', '할 일을 입력하세요.');
     return true;
 }
@@ -82,7 +82,8 @@ function insertTodoData() {
   if (!isValidate ($todoText)) return;
 
   // 새 할 일의 아이디 값을 생성하는 함수
-  const makeNewId = () => todos.length === 0 ? 1 : todos[todos.length - 1].id + 1;
+  const makeNewId = () =>
+    todos.length === 0 ? 1 : todos[todos.length - 1].id + 1;
 
   // 할 일 데이터 배열에 새로운 할 일 데이터 추가하기
 
@@ -99,12 +100,12 @@ function insertTodoData() {
 
   // 새로운 할 일 화면에 렌더링하기
   renderNewTodoElement(newTodo);
-
+  $todoText.value = '';
 }
 
 // 배열에서 삭제된 할 일 객체를 지우는 함수
 function removeTodoData(dataId) {
-  const index = todos.findIndex(todo => todo.id === dataId); // 인덱스만 가져오는 것
+  const index = todos.findIndex((todo) => todo.id === dataId); // 인덱스만 가져오는 것
   todos.splice(index, 1);
   console.log(todos);
 }
@@ -120,13 +121,58 @@ function toggleDone(dataId) {
 }
 
 // 수정 모드 진입 처리
-function toggleModifyMode ($modifyBtn) {
+function toggleModifyMode ($modifyBtn) { //modifyBtn 수정 버튼 타겟을 파라미터로 줌 !
+  // 지금 수정모드 진입상태인지 수정을해야하는 상태인지 확인
+  const isModifying = $modifyBtn.classList.contains('lnr-undo');
+  
+  // 다른 항목이 이미 수정중이면 이 함수를 종료한다.
+  if (isEnterEditMode && isModifying) return;
+  isEnterEditMode = isModifying;
+  
+  // 1. 수정 모드 진입
+  // 1-1. span.lnr-undo를 span.lnr-checkmark-circle로 클래스 교체
+  // $modifyBtn = <div class="modify"><span class="lnr lnr-undo"></span></div>
+  $modifyBtn.classList.toggle('lnr-undo'); // 수정 버튼 아이콘 껐다 켰다 해주는 toggle
+  $modifyBtn.classList.toggle('lnr-checkmark-circle');
 
+  // 부모 태그
+  const $label = $modifyBtn.parentElement.previousElementSibling;
 
+  if (isModifying) {
+  // 1-2. span.text를 input.modify-input으로 태그 교체
+  // 없어질 태그
+  const $textSpan = $label.lastElementChild;
+  // 새롭게 들어올 태그 생성
+  const $modifyInput = document.createElement('input');
+
+  $modifyInput.classList.add('modify-input');
+  $modifyInput.setAttribute('type', 'text');
+
+  $modifyInput.value = $textSpan.textContent;
+  // 부모태그.replaceChild(새롭게들어올태그, 없어질태그);
+  $label.replaceChild($modifyInput, $textSpan);
+
+} else {
+  // 1-2-1. 교체된 input태그 내부에는 기존 span의 텍스트가 그대로 들어가야함
+  // 2. 수정 완료 처리
+  // input.modify-input에 있는 value를 읽어서 새로운 span.text를 생성하여 교체
+  const $modifyInput = $label.querySelector('.modify-input');
+  const $newTextSpan = document.createElement('span');
+  $newTextSpan.classList.add('text');
+  $newTextSpan.textContent = $modifyInput.value;
+
+  $label.replaceChild($newTextSpan, $modifyInput);
+
+  // 실제 배열 데이터 수정
+  // 수정하려면 클릭한 버튼 근처 li에 있는 data-id가져와서 배열 탐색
+  const dataId = +$label.parentElement.dataset.id
+
+  const foundTodo = todos.find((todo) => todo.id === dataId); // 객체 다 가져오는 것 find
+  foundTodo.text = $newTextSpan.textContent;
+}
 }
 
 //============== 함수 실행 영역 - 함수 호출, 이벤트 리스너 등록 ===============//
-
 
 // 추가 버튼 클릭 이벤트
 document.getElementById('add').addEventListener('click', e => {
@@ -148,36 +194,21 @@ document.querySelector('.todo-list').addEventListener('click', e => {
   // document.querySelector('.todo-list').removeChild($targetLi);
   $targetLi.remove();
 
+  // 배열에서 데이터 제거
+  removeTodoData(dataId);
+
   // 체크박스 클릭시
 } else if (e.target.matches('.checkbox input[type=checkbox]')) {
+  // 체크박스를 클릭하면
+  // 체크 화면 렌더링 처리
   e.target.closest('.checkbox').classList.add('checked');
+  // 체크 데이터 처리
   toggleDone(dataId);
 
   // 수정 버튼 클릭시
 } else if (e.target.matches('.modify span')) {
-  // 수정 모드 진입
-  const $todoItem = e.target.closest('.todo-list-item');
-  // span.lnr-undo를 span.lnr-checkmark-circle로 클래스 교체 replace
-  // 클래스 교체하기 .replace('old', 'new')
-  // const $todoItem = e.target.closest('.todo-list-item');
-  // span.lnr-undo를 span.lnr-checkmark-circle로 클래스 교체
-  const $modify = $todoItem.querySelector('.modify span');
-  $modify.classList.replace('lnr-undo', 'lnr-checkmark-circle');
-
-  // span.text를 input.modify-input으로 태그 교체
-  const $modifyInput = document.createElement('input');
-  const $textSpan = $todoItem.querySelector('.text');
-  
-  $modifyInput.classList.add('modify-input');
-  //$todoItem.replaceChild($modifyInput, $textSpan);
-  $modifyInput.value = $textSpan.textContent;
-  $todoItem.replaceChild($modifyInput, $textSpan);
 
     // 수정 완료 처리
   toggleModifyMode(e.target); //'.modify span 수정 버튼을 가르킴 !
 } 
 });
-
-// 3. modify 버튼 클릭시 수정할 수 있도록 <li> 뜨는 함수
-
-// 3-1. 할 일 목록의 수정 버튼에 이벤트 리스너 추가
